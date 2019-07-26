a754101963e0d153cd9aaef6d79399a3a82cd7f4
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-25 22:36:38 -0700

    began init

--- wk1/Percolation/PercolationStats.java
+++ wk1/Percolation/PercolationStats.java
@@ -3,8 +3,31 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats
 {
+	private double average;
+	private Percolation testSite;
+
 	public PercolationStats(int n, int trials)
 	{
-		
+		double sumOfAverages = 0;
+		for (int i = 0; i < trials; i++)
+		{
+			testSite = new Percolation(n);
+			while (!testSite.percolates())
+			{
+				int rand_row = StdRandom.uniform(n) + 1;
+				int rand_col = StdRandom.uniform(n) + 1;
+				testSite.open(rand_row, rand_col);
+			}
+			sumOfAverages += (testSite.numberOfOpenSites() / (n*n));
+		}
+		average = sumOfAverages/trials;
+
+	}
+
+	public static void main(String[] args)
+	{
+		PercolationStats testStats;
+		testStats = new PercolationStats(50, 1000);
+		System.out.println(testStats.average);
 	}
 }
