12c4b60e5b185d5c9a96804aee46b22df5155b93
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-29 08:37:51 -0700

    moved to an array implementation

--- wk2/hw/RandomizedQueue.java
+++ wk2/hw/RandomizedQueue.java
@@ -7,37 +7,85 @@ import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item>
 {
-	private Node first, last;
+	private Item[] a= (Item[]) new Object[1];
+	private int head = 0;
+	private int tail = 1;
 	private int size = 0;
-
-	private class Node
-	{
-		Item item;
-		Node next;
-	}
 
 	public RandomizedQueue(){}
 
-
 	public boolean isEmpty()
-	{	return (first == null);	}
-
+	{	return (a[head] == null);	}
 
 	public int size()
-	{	return size;	}
+	{	return size;	}	
 
+	private void resize(int capacity)
+	{
+		Item[] copy = (Item[]) new Object[capacity];
+		int copyCount = 0;
+		for (int i = head; i < tail; i++)
+			if (a[i] != null)
+				copy[copyCount++] = a[i];
+		head = 0;
+		tail = copyCount;
+		a = copy;
+	}
 
 	public void enqueue(Item item)
 	{
-		oldLast = last;
-		last = new Node();
-		last.item = item;
-		last.next = null;
-		if (isEmpty()) first = last;
-		else oldLast.next = last;
+		if (tail == size) resize(size*2);
+		else a[head++] = item;
+		size++;
+	}
+	
+	private Item randomEntry(boolean remove)
+	{
+		int n = StdRandom.uniform(size);
+		Item item = a[n];
+		if (remove)
+		{
+			a[n] = null;
+			size--;
+		}
+		return item;
+	}
+		
+	public Item dequeue() {	return randomEntry(true);	}
+
+	public Item sample() {	return randomEntry(false);	}
+
+	public Iterator<Item> iterator()
+	{	return new RandomIterator();	}
+
+	private class RandomIterator implements Iterator<Item>
+	{
+		private int[] visited = new int[size];
+		private int count = 0;
+
+		public RandomIterator() {}
+		
+		public boolean hasNext()
+		{	return count == size;	}
+
+		public Item next()
+		{
+			Item entry = null;
+			while (hasNext() && entry != null)
+			{
+				int n = StdRandom.uniform(size);
+				entry = a[n];
+				visited[count++] = n;
+			}
+			return entry;
+		}
+
+		public void remove()
+		{
+			throw new UnsupportedOperationException(
+				"The remove() method is not supported");
+		}
 	}
 
-	public Item dequeue()
-	{
-		return;
-	}	
+	public static void main(String[] args){}
+}
