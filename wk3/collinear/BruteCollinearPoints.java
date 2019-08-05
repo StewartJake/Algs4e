3d1cf68ab576a5b690b5ef272bb1bbcac96ac480
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-04 18:48:45 -0700

    runs through all *txt files

--- wk3/collinear/BruteCollinearPoints.java
+++ wk3/collinear/BruteCollinearPoints.java
@@ -1,3 +1,8 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.In;
+
 public class BruteCollinearPoints
 {
     private LineSegment[] lineSegs;
@@ -5,22 +10,32 @@ public class BruteCollinearPoints
 
     public BruteCollinearPoints(Point[] points)
     {
-        lineSegs = new LineSegment[points.length];
+        lineSegs = new LineSegment[1];
         count = 0;
         int maxIter = points.length;
-        int segCounter = 0;
         
         for (int p = 0; p < maxIter - 3; p++)
-            for (int q = 0; p < maxIter - 2; q++)
-                for (int r = 0; p < maxIter - 1; r++)
-                    for (int s = 0; p < maxIter; s++)
+            for (int q = p + 1; q < maxIter - 2; q++)
+                for (int r = q + 1; r < maxIter - 1; r++)
+                    for (int s = r + 1; s < maxIter; s++)
                         if (points[p].slopeTo(points[q]) == points[q].slopeTo(points[r])
                             && points[q].slopeTo(points[r]) == points[r].slopeTo(points[s]))
                         {
-                            lineSegs[segCounter++] = lineSegment(points[p], points[r]);
-                            count++;
+                            if (count == lineSegs.length)   resize(lineSegs.length*2);
+                            lineSegs[count++] = new LineSegment(points[p], points[s]);
+                            assert lineSegs[count - 1] != null;
                         }
-                            
+        // This may cause a performace bug later
+        if (lineSegs[lineSegs.length - 1] == null)    resize(count);
+    }
+
+
+    private void resize(int capacity)
+    {
+        LineSegment[] copy = new LineSegment[capacity];
+        for (int i = 0; i < count; i++)
+            copy[i] = lineSegs[i];
+        lineSegs = copy;
     }
   
    
@@ -28,6 +43,36 @@ public class BruteCollinearPoints
    {    return count;   }
 
 
-    public LineSegment[] segment()
-    {   return LineSegment; }
+    public LineSegment[] segments()
+    {   return lineSegs; }
+    
+    public static void main(String[] args) {
+
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+        int x = in.readInt();
+        int y = in.readInt();
+        points[i] = new Point(x, y);
+    }
+
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+        p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+        StdOut.println(segment);
+        segment.draw();
+    }
+    StdDraw.show();
+    }
 }           
