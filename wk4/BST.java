98dd9a64918d848235931623657b395aba158e75
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-17 08:40:34 -0700

    Hibbard deletion

--- wk4/BST.java
+++ wk4/BST.java
@@ -135,4 +135,29 @@ public class BST<Key extaneds Comparable<Key>, Value>
         x.count = 1 + size(x.left) + size(x.right);
         return x;
     }
+
+
+    private void delete(Key key)
+    {   root = delete(root, key);   }
+
+
+    private node delete(Node x, Key key)
+    {
+        if (x == null)  return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left    - delete(x.left,    key);
+        else if (cmp > 0) x.right   - delete(x.right,   key);
+        else
+        {
+            if (x.right == null) return x.left;
+            if (x.left  == null) return x.right;
+
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.count = size(x.left) + size(x.right) + 1;
+        return x;
+    }
 }
