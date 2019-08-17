48ce5be5ea345f16b98bcb992a115c7d2a6b41fc
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-17 11:40:47 -0700

    first commit

---
+++ wk4/puzzle/Board.java
@@ -0,0 +1,44 @@
+public class Board
+{
+
+    public Board(int[][] tiles)
+    {}
+
+
+    public String toString()
+    {}
+
+
+    public int dimension()
+    {}
+
+
+    public int hamming()
+    {}
+
+
+    public int manhattten()
+    {}
+
+
+    public boolean isGoal()
+    {}
+
+
+    public boolean equals(Object y)
+    {}
+
+
+    public Iterable<Board> neighbors()
+    {}
+
+
+    public Board twin()
+    {}
+
+
+    public static void main(String[] args)
+    {}
+
+
+}
---
+++ wk4/puzzle/README.md
@@ -0,0 +1,1 @@
+A 3x3 slider puzzle with 8 tiles and an open space
