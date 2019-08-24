5a21495801e899c65d23ffcefe1def6e79a71bb2
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-24 09:20:47 -0700

    API outline

---
+++ wk5/kdTrees/KdTree.java
@@ -0,0 +1,39 @@
+import java.Util.TreeSet
+
+public class KdTree
+{
+    public KdTree()
+    {}
+
+
+    public boolean isEmpty()
+    {return false;}
+
+
+    public int size()
+    {return 0;}
+
+
+    public void insert(Point2D p)
+    {}
+
+
+    public boolean contains(Point2D p)
+    {return false;}
+
+
+    public void draw()
+    {}
+
+
+    // public Iterable<Point2D> range(RectHV rect)
+    // {}
+
+
+    // public Point2D nearest(Point2D p)
+    // {}
+
+
+    public static void main(String[] args)
+    {}
+}
