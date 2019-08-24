43e07f326eb05fbda5741cafc765cfad33a1475f
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-24 11:56:10 -0700

    started implementation

--- wk5/kdTrees/KdTree.java
+++ wk5/kdTrees/KdTree.java
@@ -1,21 +1,74 @@
-import java.Util.TreeSet
+import edu.princeton.cs.algs4.Point2D;
 
 public class KdTree
 {
-    public KdTree()
-    {}
+    Node    root;
 
+    public KdTree()
+    {
+        this.size = 0;
+        this.root = null;
+    }
+
+    private class Node
+    {
+        private int     count;
+        private boolean vertical = true;
+        private double  key;
+        private Point2D val;
+        private Node    prev;
+        private Node    lebo = null;   // left-bottom
+        private Node    rito = null;   // right-top
+
+        Node(Node previous, Point2D p)
+        {
+            if (previous == null)
+                this.prev = null;
+            else
+            {
+                this.prev = previous;
+                if (previous.vertical)
+                {
+                    this.vertical = false;
+                    this.key = p.x();
+                }
+                else
+                    this.key = p.y();
+            }
+            this.val = p;
+        }
+    }
 
     public boolean isEmpty()
-    {return false;}
+    {   return size == 0;   }
 
 
     public int size()
-    {return 0;}
+    {
+        if (root == null)    return 0;
+        return x.count;
+    }
 
 
     public void insert(Point2D p)
-    {}
+    {
+            
+    }
+
+    private Node put(Node query, Point2D p)
+    {
+        if (query == null) return new Node(null, p);
+        if (query.vertical)
+        {
+            if      (query.val.x > p.val.x) (n.rito;
+            else if (query.val.x < p.val.x) n.lebo;
+            else                            n;
+        }
+        if (!n.vertical)
+        {
+            if      (query.val.y > n.val.y) return n.rito;
+            else if (query.val.y < n.val.y) return n.lebo;
+            else                            return n; 
 
 
     public boolean contains(Point2D p)
