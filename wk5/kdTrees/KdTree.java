a4bd6f9a97fff2310da18083f79a93f49905e4b4
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-24 16:53:15 -0700

    implementing

--- wk5/kdTrees/KdTree.java
+++ wk5/kdTrees/KdTree.java
@@ -2,39 +2,30 @@ import edu.princeton.cs.algs4.Point2D;
 
 public class KdTree
 {
+    int     size;
     Node    root;
 
     public KdTree()
     {
         this.size = 0;
-        this.root = null;
     }
 
     private class Node
     {
-        private int     count;
-        private boolean vertical = true;
+        private boolean vertical;
         private double  key;
         private Point2D val;
+        private Node    lebo;   // left-bottom
+        private Node    rito;   // right-top
         private Node    prev;
-        private Node    lebo = null;   // left-bottom
-        private Node    rito = null;   // right-top
 
-        Node(Node previous, Point2D p)
+        Node(Point2D p, Node prev)
         {
-            if (previous == null)
-                this.prev = null;
+            if (prev == null || prev.vertical)
+                this.key = p.x();
             else
-            {
-                this.prev = previous;
-                if (previous.vertical)
-                {
-                    this.vertical = false;
-                    this.key = p.x();
-                }
-                else
-                    this.key = p.y();
-            }
+                this.key = p.y();
+            this.vertical = (prev == null) ? true : !prev.vertical;
             this.val = p;
         }
     }
@@ -44,35 +35,57 @@ public class KdTree
 
 
     public int size()
-    {
-        if (root == null)    return 0;
-        return x.count;
-    }
+    {   return size;    }
 
 
     public void insert(Point2D p)
     {
-            
+        root = put(root, p, root);
     }
 
-    private Node put(Node query, Point2D p)
+
+    private Node put(Node x, Point2D p, Node prev)
     {
-        if (query == null) return new Node(null, p);
-        if (query.vertical)
+        if (x == null)
         {
-            if      (query.val.x > p.val.x) (n.rito;
-            else if (query.val.x < p.val.x) n.lebo;
-            else                            n;
+            size++;
+            return new Node(p, prev);
         }
-        if (!n.vertical)
+        if (p.equals(x.val))    x.val = p;
+        if (x.vertical)
         {
-            if      (query.val.y > n.val.y) return n.rito;
-            else if (query.val.y < n.val.y) return n.lebo;
-            else                            return n; 
+            if (p.x() > x.key)  x = put(x.rito, p, x);
+            if (p.x() < x.key)  x = put(x.lebo, p, x);
+        }
+        if (!x.vertical)
+        {
+            if (p.y() > x.key)  x = put(x.rito, p, x);
+            if (p.y() < x.key)  x = put(x.lebo, p, x);
+        }
+        return x;
+    }
+
 
 
     public boolean contains(Point2D p)
-    {return false;}
+    {
+        Node x = root;
+        while(x != null)
+        {
+            if (p.equals(x.val)) return true;
+            if (x.vertical)
+            {
+                if      (p.x() > x.val.x())  x = x.rito;
+                else if (p.x() < x.val.x())  x = x.lebo;
+            }
+            else
+            {
+                if      (p.y() > x.val.y())  x = x.rito;
+                else if (p.y() < x.val.y())  x = x.lebo;
+            }
+        }
+        return false;
+    }
 
 
     public void draw()
@@ -88,5 +101,17 @@ public class KdTree
 
 
     public static void main(String[] args)
-    {}
+    {
+        Point2D p0 = new Point2D(0.0, 0.0);
+        Point2D p1 = new Point2D(1.0, 1.0);
+        Point2D p2 = new Point2D(2.0, 2.0);
+        Point2D p3 = new Point2D(3.0, 3.0);
+        Point2D p4 = new Point2D(4.0, 4.0);
+        Point2D p5 = new Point2D(5.0, 5.0);
+        KdTree kt = new KdTree();
+        kt.insert(p0);
+        kt.insert(p2);
+        System.out.println(kt.contains(p3));
+        System.out.println(kt.contains(p2));
+    }
 }
