0f95a0a1458cf40e88c792e4beb87aba0a099664
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-26 07:03:05 -0700

    63

--- wk5/kdTrees/KdTree.java
+++ wk5/kdTrees/KdTree.java
@@ -17,6 +17,7 @@ public class KdTree
     private class Node
     {
         private final boolean   vertical;
+        private int             count;
         private double          key;
         private Point2D         val;
         private Node            lebo;   // left-bottom
@@ -45,11 +46,15 @@ public class KdTree
     }
 
     public boolean isEmpty()
-    {   return size == 0;   }
+    {   return size(root) == 0;   }
 
 
     public int size()
-    {   return size;    }
+    {   return size(root);    }
+
+
+    private int size(Node x)
+    {   return (x == null) ? 0 : x.count;   }
 
 
     public void insert(Point2D p)
@@ -62,15 +67,9 @@ public class KdTree
 
     private Node put(Node x, Point2D p, Node prev)
     {
-        if (x == null)
-        {
-            size++;
-            // Node n = new Node(p, prev);
-            // pQ.add(n);
-            return new Node(p, prev);
-        }
-        if (p.equals(x.val))    return x;
-        if (x.vertical)
+        if (x == null)  x = new Node(p, prev);
+        // if (p.equals(x.val))    return x;
+        else if (x.vertical)
         {
             if (p.x() >=    x.key)  x.rito = put(x.rito, p, x);
             if (p.x() <     x.key)  x.lebo = put(x.lebo, p, x);
@@ -80,6 +79,7 @@ public class KdTree
             if (p.y() >=    x.key)  x.rito = put(x.rito, p, x);
             if (p.y() <     x.key)  x.lebo = put(x.lebo, p, x);
         }
+        x.count = 1 + size(x.lebo) + size(x.rito);
         return x;
     }
 
@@ -150,7 +150,7 @@ public class KdTree
             if      (x.vertical)
             {
                 if      (rect.xmax() <  x.key)   range(x.lebo, rect, rQ);
-                else if (rect.xmax() >= x.key)   range(x.rito, rect, rQ);
+                else if (rect.xmin() >= x.key)   range(x.rito, rect, rQ);
                 else if (rect.xmin() <  x.key && rect.xmax() > x.key)
                 {
                     range(x.lebo, rect, rQ);
@@ -160,7 +160,7 @@ public class KdTree
             else    // !x.vertical
             {
                 if      (rect.ymax() <  x.key)   range(x.lebo, rect, rQ);
-                else if (rect.ymax() >= x.key)   range(x.rito, rect, rQ);
+                else if (rect.ymin() >= x.key)   range(x.rito, rect, rQ);
                 else if (rect.ymin() <  x.key && rect.ymax() > x.key)
                 {
                     range(x.lebo, rect, rQ);
@@ -185,28 +185,28 @@ public class KdTree
         if (contender != null)
         {
             if (contender.val.distanceSquaredTo(p)
-                    < champ.distanceSquaredTo(p))
+                    <= champ.distanceSquaredTo(p))
                 champ = contender.val;
             if ((contender.lebo != null && contender.rito != null)
                     &&  contender.lebo.val.distanceSquaredTo(p) 
-                    <   contender.rito.val.distanceSquaredTo(p))
+                    <=   contender.rito.val.distanceSquaredTo(p))
             {
-                if (contender.lebo.val.distanceSquaredTo(p)
-                        < champ.distanceSquaredTo(p))
+                //if (contender.lebo.val.distanceSquaredTo(p)
+                //        <= champ.distanceSquaredTo(p))
                     champ = nearest(contender.lebo, champ, p);
-                if (contender.rito.val.distanceSquaredTo(p)
-                        < champ.distanceSquaredTo(p))
+                // if (contender.rito.val.distanceSquaredTo(p)
+                //         <= champ.distanceSquaredTo(p))
                     champ = nearest(contender.rito, champ, p);
             }
             else
             {
-                if (contender.rito != null 
-                        && (contender.rito.val.distanceSquaredTo(p)
-                            < champ.distanceSquaredTo(p)))
+                // if (contender.rito != null 
+                //         && (contender.rito.val.distanceSquaredTo(p)
+                //             <= champ.distanceSquaredTo(p)))
                     champ = nearest(contender.rito, champ, p);
-                if (contender.lebo != null 
-                        && (contender.lebo.val.distanceSquaredTo(p)
-                            < champ.distanceSquaredTo(p)))
+                // if (contender.lebo != null 
+                //         && (contender.lebo.val.distanceSquaredTo(p)
+                //             <= champ.distanceSquaredTo(p)))
                     champ = nearest(contender.lebo, champ, p);
             }
         }
@@ -219,29 +219,29 @@ public class KdTree
         RectHV rect = new RectHV(0.25, 0.125, 0.5625, 1.0); 
         // for (Node n : kt.pQ)
         //     System.out.println(n);
-        // System.out.println(kt.range(rect));
-        // System.out.println(kt.nearest(p0));
-        Point2D A = new Point2D(0.3125, 0.75);
-        Point2D B = new Point2D(0.1875, 0.8125);
-        Point2D C = new Point2D(0.125,  0.5);
-        Point2D D = new Point2D(0.9375, 0.1875);
-        Point2D E = new Point2D(0.6875, 0.0625);
-        Point2D F = new Point2D(0.375,  0.0);
-        Point2D G = new Point2D(0.5,    0.625);
-        Point2D H = new Point2D(1.0,    0.25);
-        Point2D I = new Point2D(0.875,  0.4375);
-        Point2D J = new Point2D(0.0625, 0.5625);
+        Point2D A = new Point2D(0.7,    0.2);
+        Point2D B = new Point2D(0.5,    0.4);
+        Point2D C = new Point2D(0.2,    0.3);
+        Point2D D = new Point2D(0.4,    0.7);
+        Point2D E = new Point2D(0.9,    0.6);
+        Point2D F = new Point2D(0.646,  0.899);
+        // Point2D G = new Point2D(0.5,    0.625);
+        // Point2D H = new Point2D(1.0,    0.25);
+        // Point2D I = new Point2D(0.875,  0.4375);
+        // Point2D J = new Point2D(0.0625, 0.5625);
         kt.insert(A);
         kt.insert(B);
         kt.insert(C);
         kt.insert(D);
         kt.insert(E);
-        kt.insert(F);
-        kt.insert(G);
-        kt.insert(H);
-        kt.insert(I);
-        kt.insert(J);
-        System.out.println(rect);
-        System.out.println(kt.range(rect));
+        // kt.insert(F);
+        // kt.insert(G);
+        // kt.insert(H);
+        // kt.insert(I);
+        // kt.insert(J);
+        // System.out.println(rect);
+        // System.out.println(kt.range(rect));
+        System.out.println(kt.nearest(F));
+        System.out.println(kt.size());
     }
 }
