2f13c3f18cf7459a96d795b51232df2bc6ae49ad
Jacob Stewart <jacobstewart1987@gmail.com>
2019-08-29 19:39:12 -0700

    LL implementation

--- wk6/SeparateChainingHashST.java
+++ wk6/SeparateChainingHashST.java
@@ -17,4 +17,12 @@ public class SeparateChainingHashST<Key, Value>
             if (key.equals(x.key)) return (Value) x.val;
         return null;
     }
+
+    public Value put(Key key, Value val)
+    {
+        int i = hash(key);
+        for (Node x = st[i]; x != null; x = x.next)
+            if (key.equals(x.key))  {   x.val = val; return;    }
+        st[i] = new Node(key, val, st[i]);
+    }
 }
